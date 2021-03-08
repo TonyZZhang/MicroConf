083d031317fab86b3e29b7c0375d2757747b8353
zhanghaizhou <zhanghaizhou.it@haier.com>
2021-03-08 16:53:03 +0800

    增加watcher

--- ConfigService/watcher/watcher.go
+++ ConfigService/watcher/watcher.go
@@ -1,5 +1,33 @@
 package watcher
 
+import (
+	"errors"
+)
+
+var ErrWatcherStopped = errors.New("watcher stopped")
+
 type Watcher struct {
-	exit chan interface{}
+	exit    chan interface{}
+	updates chan *ConfigResponse
+}
+
+type ConfigResponse struct {
+}
+
+func (w *Watcher) Next() (*ConfigResponse, error) {
+	select {
+	case <-w.exit:
+		return nil, ErrWatcherStopped
+	case v := <-w.updates:
+		return v, nil
+	}
+}
+
+func (w *Watcher) Stop() error {
+	select {
+	case <-w.exit:
+	default:
+		close(w.exit)
+	}
+	return nil
 }
